cd919fe326b4762007a1190fa150fabc6f8f37bc
YesicaChui <yesicachuic@gmail.com>
2023-09-19 03:03:33 -0500

    creando toolbar,menu lateral y contenido en un layout usando grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AsideComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
